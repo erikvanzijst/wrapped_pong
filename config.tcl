41cdd8cad3c281c723b64981803295b8960bab01
Matt Venn <matt@mattvenn.net>
2021-12-08 13:58:41 +0100

    fix for mpw4

--- config.tcl
+++ config.tcl
@@ -42,8 +42,8 @@ set ::env(GLB_RT_MAXLAYER) 5
 set ::env(PL_RESIZER_BUFFER_OUTPUT_PORTS) 0
 
 # define power straps so the macro works inside Caravel's PDN
-set ::env(VDD_NETS) [list {vccd1} {vccd2} {vdda1} {vdda2}]
-set ::env(GND_NETS) [list {vssd1} {vssd2} {vssa1} {vssa2}]
+set ::env(VDD_NETS) [list {vccd1}]
+set ::env(GND_NETS) [list {vssd1}]
 
 # regular pin order seems to help with aggregating all the macros for the group project
 set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
